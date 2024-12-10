8b826d243608064b73f6858c959bb1bcf377d330
prinshu5520 <prinshu5520@gmail.com>
2024-12-10 16:43:22 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from ir_ocr import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)  # You can set debug to False in production
+    app.run(debug=False, host='0.0.0.0')
+ 
